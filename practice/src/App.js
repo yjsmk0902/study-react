47bc9fe2da990ceec6364b8f0ebc19d03530e3de
Luke_YS <yjsmk0902@gmail.com>
2023-03-16 10:37:41 +0900

    Movie App practice

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,5 +1,48 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div></div>;
+  const url = `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`;
+
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (await fetch(url)).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>로딩중...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summery}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
