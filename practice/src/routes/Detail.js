ed661aeff3512ed268c72c85683f38a2c9a35b5c
Luke_YS <yjsmk0902@gmail.com>
2023-03-17 11:37:57 +0900

    [Movie App] Params

--- practice/src/routes/Detail.js
+++ practice/src/routes/Detail.js
@@ -1,4 +1,15 @@
+import { useParams } from "react-router-dom";
+
 function Detail() {
+  const id = useParams();
+  const url = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`;
+  const getMovies = async () => {
+    const json = await (await fetch(url)).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovies();
+  });
   return <div>Detail</div>;
 }
 
