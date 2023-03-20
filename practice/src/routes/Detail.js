743fdbd8cf671254f91f1f1e74c8536b9643305e
Luke_YS <yjsmk0902@gmail.com>
2023-03-20 11:13:25 +0900

    [Movie App] 배포

--- practice/src/routes/Detail.js
+++ practice/src/routes/Detail.js
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 
+import { useEffect } from "react";
+
 function Detail() {
   const id = useParams();
   const url = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`;
